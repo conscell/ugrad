77039ae77881ed0f99236ffc00d5ac5469921ad9
conscell <conscell@users.noreply.github.com>
2024-08-22 16:20:05 +0900

    add conv2d example

---
+++ example_conv2d.py
@@ -0,0 +1,95 @@
+import os
+os.environ['UGRAD_COMPUTE'] = 'cupy'
+import cupy as np
+
+import ugrad
+import ugrad.nn as nn
+import ugrad.nn.functional as F
+
+def main():
+    seed = 1717
+    np.random.seed(seed)
+    num_epochs = 10
+    batch_size = 32
+
+    from sklearn.datasets import fetch_openml
+    from sklearn.model_selection import train_test_split
+    
+    X, y = fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False, parser='liac-arff')
+    X = X.reshape(-1, 1, 28, 28)
+    y = y.astype(np.int64)
+    # Scale images to the [0, 1] range
+    X /= 255.
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+    print(f"{X_train.shape=} {X_test.shape=} {y_train.shape=} {y_test.shape=}")
+
+    inputs_t = ugrad.Tensor(X_test)
+    labels_t = np.array(y_test)
+    print(f"{labels_t[0]=}")
+
+    class Model(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.conv_l1 = nn.Conv2d(1, 32, (3, 3), name='conv_l1')
+            self.conv_l2 = nn.Conv2d(32, 64, (3, 3), name='conv_l2')
+            self.avg_pool = nn.AvgPool2d((2, 2), name='avg_pool')
+            self.l1 = nn.Linear(64 * 12 * 12, 128, name='l1')
+            self.l2 = nn.Linear(128, 10, name='l2')
+
+        def forward(self, x):
+            z = F.relu(self.conv_l1(x))
+            z = F.relu(self.conv_l2(z))
+            z = self.avg_pool(z)
+            z = F.dropout(z, p=0.25, training=self.training)
+            z = z.reshape((-1, 64 * 12 * 12))
+            z = F.relu(self.l1(z))
+            z = F.dropout(z, p=0.5, training=self.training)
+            out = self.l2(z)
+            return out
+
+    model = Model()
+    optimizer = ugrad.optim.Adam(model.parameters(), lr=1e-3)
+    scheduler = ugrad.optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=1e-2, total_iters=num_epochs)
+
+    num_batches = -(-X_train.shape[0] // batch_size)
+
+    for k in range(num_epochs):
+
+        accuracy = 0
+        train_loss = 0
+        model.train()
+        for batch in range(num_batches):
+            inputs = ugrad.Tensor(X_train[batch * batch_size:(batch + 1) * batch_size])
+            labels = np.array(y_train[batch * batch_size:(batch + 1) * batch_size])
+
+            # Forward
+            preds = model(inputs)
+            loss = F.cross_entropy(preds, labels)
+
+            # Backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            # Update (Adam)
+            optimizer.step()
+
+            accuracy += int(np.count_nonzero(np.argmax(preds.data, axis=-1) == labels))
+            train_loss += loss.data.item()
+
+        scheduler.step()
+        accuracy /= X_train.shape[0]
+        train_loss /= X_train.shape[0]
+        
+        model.eval()
+        with ugrad.no_grad():
+            preds_t = model(inputs_t)
+            loss_t = F.cross_entropy(preds_t, labels_t).data.item()
+
+        accuracy_t = int(np.count_nonzero(np.argmax(preds_t.data, axis=-1) == labels_t)) / X_test.shape[0]
+
+        print(f"Epoch {k+1} loss {train_loss:.6f}, accuracy {accuracy * 100:.6f}%  test loss {loss_t:.6f}, test accuracy {accuracy_t * 100:.6f}% lr {optimizer.lr:.6f}")
+
+
+if __name__ == "__main__":
+    main()
