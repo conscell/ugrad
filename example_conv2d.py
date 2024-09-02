11cf140a109e8db2664e13c7a7b6deeb1c6345e6
conscell <conscell@users.noreply.github.com>
2024-09-02 16:12:15 +0900

    vectorized implementation of conv2d and avg_pool2d

--- example_conv2d.py
+++ example_conv2d.py
@@ -10,7 +10,7 @@ def main():
     seed = 1717
     np.random.seed(seed)
     num_epochs = 10
-    batch_size = 32
+    batch_size = 128
 
     from sklearn.datasets import fetch_openml
     from sklearn.model_selection import train_test_split
@@ -24,9 +24,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
     print(f"{X_train.shape=} {X_test.shape=} {y_train.shape=} {y_test.shape=}")
 
-    inputs_t = ugrad.Tensor(X_test)
-    labels_t = np.array(y_test)
-    print(f"{labels_t[0]=}")
+    print(f"{y_test[0]=}")
 
     class Model(nn.Module):
         def __init__(self):
@@ -53,6 +51,7 @@ def main():
     scheduler = ugrad.optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=1e-2, total_iters=num_epochs)
 
     num_batches = -(-X_train.shape[0] // batch_size)
+    num_batches_t = -(-X_test.shape[0] // batch_size)
 
     for k in range(num_epochs):
 
@@ -81,12 +80,20 @@ def main():
         accuracy /= X_train.shape[0]
         train_loss /= X_train.shape[0]
         
+        accuracy_t = 0
+        loss_t = 0
         model.eval()
         with ugrad.no_grad():
-            preds_t = model(inputs_t)
-            loss_t = F.cross_entropy(preds_t, labels_t).data.item()
-
-        accuracy_t = int(np.count_nonzero(np.argmax(preds_t.data, axis=-1) == labels_t)) / X_test.shape[0]
+            for batch in range(num_batches_t):
+                inputs_t = ugrad.Tensor(X_test[batch * batch_size:(batch + 1) * batch_size])
+                labels_t = np.array(y_test[batch * batch_size:(batch + 1) * batch_size])
+                
+                preds_t = model(inputs_t)
+                loss_t += F.cross_entropy(preds_t, labels_t).data.item()
+                accuracy_t += int(np.count_nonzero(np.argmax(preds_t.data, axis=-1) == labels_t))
+        
+        accuracy_t /= X_test.shape[0]
+        loss_t /= X_test.shape[0]
 
         print(f"Epoch {k+1} loss {train_loss:.6f}, accuracy {accuracy * 100:.6f}%  test loss {loss_t:.6f}, test accuracy {accuracy_t * 100:.6f}% lr {optimizer.lr:.6f}")
 
--- ugrad/nn/functional.py
+++ ugrad/nn/functional.py
@@ -98,25 +98,41 @@ def conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1):
     else:
         x = input.data
 
-    result = Tensor(np.zeros((N, C_out, H_out, W_out)), name="conv2d")
+    # Naïve implementation
+    #
+    # result = Tensor(np.zeros((N, C_out, H_out, W_out)), name="conv2d")
+    #
+    # for i in range(H_out):
+    #     for j in range(W_out):
+    #         result.data[...,i, j] = np.sum(weight.data * 
+    #                                        np.expand_dims(x[...,i * stride[0] : i * stride[0] + dilated_size[0] : dilation[0],
+    #                                                             j * stride[1] : j * stride[1] + dilated_size[1] : dilation[1]], axis=-4),
+    #                                 axis=(-3, -2, -1))
 
-    for i in range(H_out):
-        for j in range(W_out):
-            result.data[...,i, j] = np.sum(weight.data * 
-                                           np.expand_dims(x[...,i * stride[0] : i * stride[0] + dilated_size[0] : dilation[0],
-                                                                j * stride[1] : j * stride[1] + dilated_size[1] : dilation[1]], axis=-4),
-                                      axis=(-3, -2, -1))
+    x_strided = np.lib.stride_tricks.sliding_window_view(
+            x,
+            dilated_size,
+            axis=(-2, -1)
+        )[..., ::stride[0] ,::stride[1], ::dilation[0], ::dilation[1]]
+    
+    result = Tensor(np.einsum("oikl, nihwkl -> nohw", weight.data, x_strided), name="conv2d")
     
     if weight.requires_grad and weight.grad_enabled:
-        def grad_fn(grad): 
-            weight_grad = np.zeros_like(weight.data)
+        def grad_fn(grad):
             
-            for i in range(H_out):
-                for j in range(W_out):
-                    weight_grad += np.sum(np.expand_dims(grad[...,i, j], axis=(-3, -2, -1)) * 
-                                          np.expand_dims(x[...,i * stride[0] : i * stride[0] + dilated_size[0] : dilation[0], 
-                                                               j * stride[1] : j * stride[1] + dilated_size[1] : dilation[1]], axis=-4), 
-                                    axis=0)
+            # Naïve implementation
+            #
+            # weight_grad = np.zeros_like(weight.data)
+            #
+            # for i in range(H_out):
+            #     for j in range(W_out):
+            #         weight_grad += np.sum(np.expand_dims(grad[...,i, j], axis=(-3, -2, -1)) * 
+            #                               np.expand_dims(x[...,i * stride[0] : i * stride[0] + dilated_size[0] : dilation[0], 
+            #                                                    j * stride[1] : j * stride[1] + dilated_size[1] : dilation[1]], axis=-4), 
+            #                         axis=0)
+
+            weight_grad = np.einsum("nohw, nihwkl -> oikl", grad, x_strided) if weight.requires_grad else None
+
             return (weight_grad, )
 
         result.grad_fn = Node(grad_fn=grad_fn,
@@ -152,24 +168,46 @@ def avg_pool2d(input, kernel_size, stride=None, padding=0):
     else:
         x = input.data
 
-    result = Tensor(np.zeros((N, C, H_out, W_out)), name="avg_pool2d")
+    # Naïve implementation
+    #
+    # result = Tensor(np.zeros((N, C, H_out, W_out)), name="avg_pool2d")
 
-    for i in range(H_out):
-        for j in range(W_out):
-            result.data[...,i, j] = np.average(x[...,i * stride[0] : i * stride[0] + kernel_size[0],
-                                                     j * stride[1] : j * stride[1] + kernel_size[1]], axis=(-2, -1))
+    # for i in range(H_out):
+    #     for j in range(W_out):
+    #         result.data[...,i, j] = np.average(x[...,i * stride[0] : i * stride[0] + kernel_size[0],
+    #                                                 j * stride[1] : j * stride[1] + kernel_size[1]], axis=(-2, -1))
+
+    result = Tensor(np.average(
+        np.lib.stride_tricks.sliding_window_view(x, kernel_size, axis=(-2, -1))[..., ::stride[0] ,::stride[1], :, :],
+        axis=(-2, -1)
+        ), name="avg_pool2d")
     
     if input.requires_grad and input.grad_enabled:
         def grad_fn(grad):
             kernel_numel = kernel_size[0] * kernel_size[1]
             
-            input_grad = np.zeros_like(x)
+            # Naïve implementation
+            #
+            # input_grad = np.zeros_like(x)
+            #
+            # for i in range(H_out):
+            #     for j in range(W_out):
+            #         input_grad[...,i * stride[0] : i * stride[0] + kernel_size[0],
+            #                        j * stride[1] : j * stride[1] + kernel_size[1]] += np.expand_dims(grad[...,i, j], 
+            #                                                                                          axis=(-2, -1)) / kernel_numel
+
+            input_grad = np.zeros(grad.shape[:-2] + ((grad.shape[-2] - 1) * stride[0] + 2 * kernel_size[0] - 1, 
+                                                     (grad.shape[-1] - 1) * stride[1] + 2 * kernel_size[1] - 1))
             
-            for i in range(H_out):
-                for j in range(W_out):
-                    input_grad[...,i * stride[0] : i * stride[0] + kernel_size[0],
-                                   j * stride[1] : j * stride[1] + kernel_size[1]] += np.expand_dims(grad[...,i, j], 
-                                                                                                     axis=(-2, -1)) / kernel_numel
+            input_grad[..., kernel_size[0] - 1 : -kernel_size[0] + 1 : stride[0], 
+                            kernel_size[1] - 1 : -kernel_size[1] + 1 : stride[1]] = grad
+            
+            input_grad = np.sum(np.lib.stride_tricks.sliding_window_view(input_grad, kernel_size, axis=(-2, -1)), axis=(-2, -1)) / kernel_numel
+            
+            input_grad = np.pad(input_grad, pad_width=((0, 0), 
+                                                       (0, 0), 
+                                                       (0, max(x.shape[-2] - input_grad.shape[-2], 0)),
+                                                       (0, max(x.shape[-1] - input_grad.shape[-1], 0))))
             if padding:
                 return (input_grad[...,padding[0] : H_in + padding[0], padding[1] : W_in + padding[1]], )
             
